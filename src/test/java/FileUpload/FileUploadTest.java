ec9384b4e6459dfc77d5fe9523abebc24a50c4a8
Denis Hoefel <denishoefel@gmail.com>
2024-06-10 20:24:34 -0300

    FileUploadTest updated

--- src/test/java/FileUpload/FileUploadTest.java
+++ src/test/java/FileUpload/FileUploadTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FileUploadTest extends BaseTests {
-
+//This test case is not working anymore. Need to fix.
     @Test
     public void testUploadFiles(){
         var uploadPage= homePage.clickFileUpload();
