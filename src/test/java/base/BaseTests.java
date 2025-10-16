b7583fb0cb8fa8848013da05098f580a11005b3c
i841937 <denishoefel@sap.com>
2025-10-16 15:45:45 -0300

    updates github

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,27 +1,36 @@
 package base;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import pages.HomePage;
-
+import pages.HomePage; // importe o seu pacote correto
 
 public class BaseTests {
-
-    private WebDriver driver;
+    protected WebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
-    public void setUp () {
-       // System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe"); //informing the webdriver and location
-        driver = new ChromeDriver(); //instantiate ChromeDriver
-        driver.get("https://the-internet.herokuapp.com//"); //access to the page you want to test
-        driver.manage().window().maximize(); //maximize the window
-        homePage = new HomePage(driver); //the object homePage will receive the URL so you can start the tests from there
-    }
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        if ("true".equalsIgnoreCase(System.getenv("CI"))
+                || "true".equalsIgnoreCase(System.getenv("HEADLESS"))) {
+            options.addArguments("--headless=new", "--no-sandbox",
+                    "--disable-dev-shm-usage", "--window-size=1920,1080",
+                    "--remote-allow-origins=*");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        // Navega para a Home e instancia o Page Object
+        driver.get("https://seu-site-ou-app-under-test.com");
+        homePage = new HomePage(driver);
     }
 
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
 }
